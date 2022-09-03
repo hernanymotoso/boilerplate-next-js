78d3299d98ca040b443c71db6d3cc6af05bd36ec
Hernany Motoso <contatohernany@gmail.com>
2022-09-03 18:20:56 -0300

    chore: add and config prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
     return (
@@ -20,7 +20,7 @@ export default function Home() {
                 </h1>
 
                 <p className={styles.description}>
-                    Get started by editing{" "}
+                    Get started by editing{' '}
                     <code className={styles.code}>pages/index.js</code>
                 </p>
 
@@ -71,7 +71,7 @@ export default function Home() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Powered by{" "}
+                    Powered by{' '}
                     <span className={styles.logo}>
                         <Image
                             src="/vercel.svg"
