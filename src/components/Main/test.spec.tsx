03b7a82e6487e2f0cf2b8eb2481abb0314db34ed
Hernany Motoso <contatohernany@gmail.com>
2022-09-04 17:44:14 -0300

    test(main): Ensures render colors correctly

--- src/components/Main/test.spec.tsx
+++ src/components/Main/test.spec.tsx
@@ -3,13 +3,19 @@ import { render, screen } from '@testing-library/react';
 import Main from '.';
 
 describe('<Main/>', () => {
-    it('Should render the heading', () => {
-        const { container } = render(<Main />);
+  it('Should render the heading', () => {
+    const { container } = render(<Main />);
 
-        expect(
-            screen.getByRole('heading', { name: /react avançado/i }),
-        ).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i }),
+    ).toBeInTheDocument();
 
-        expect(container.firstChild).toMatchSnapshot();
-    });
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('Should render colors correctly', () => {
+    const { container } = render(<Main />);
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' });
+  });
 });
