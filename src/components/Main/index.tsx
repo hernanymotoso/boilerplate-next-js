d56a69885682a1bc5126c9fd4c348d43b708e944
Hernany Motoso <contatohernany@gmail.com>
2022-09-04 19:17:27 -0300

    chore: add and config storybook, add addon-knobs and make config to styled-components work with storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as S from './styles';
 
-const Main = () => (
+const Main = ({
+  title = 'React avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components',
+}) => (
   <S.Wrapper>
     <S.Logo
       src="/img/logo.svg"
       alt="Imagem de um átomo e React Avançado escrito ao lado."
     />
-    <S.Title>React avançado</S.Title>
-    <S.Description>
-      TypeScript, ReactJS, NextJS e Styled Components
-    </S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
     <S.Illustration
       src="/img/hero-illustration.svg"
       alt="desenvolvedor de frente para tela com código"
